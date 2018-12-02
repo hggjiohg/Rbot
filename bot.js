5659f95f4a44bafb541fe10c6605a87d1c2c02df
hggjiohg <44272381+hggjiohg@users.noreply.github.com>
2018-12-02 16:11:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -217,7 +217,7 @@ client.on('guildMemberAdd', member => {
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
     const logChannel = member.guild.channels.find(channel => channel.name === "rinv");
-    logChannel.send(`Invited by: <@${inviter.tag}>`);
+    msg.channel.send('Invited by: <@${inviter.tag}>');
   });
 });
 
