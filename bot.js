39bfeeb94ec7884a034497b10515984abe8465d8
hggjiohg <44272381+hggjiohg@users.noreply.github.com>
2018-12-02 17:27:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -217,7 +217,7 @@ client.on('guildMemberAdd', member => {
     const inviter = client.users.get(invite.inviter.id);
     const stewart = member.guild.channels.find("name", "rinv");
      stewart.send(`<@${member.user.id}> invited by <@${inviter.id}>`);
-    stewart.send(`<@${member.user.id}> joined using invite code https://discord.gg/RahPaB${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
+    //stewart.send(`<@${member.user.id}> joined using invite code https://discord.gg/RahPaB${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
   }); 
 });
 
